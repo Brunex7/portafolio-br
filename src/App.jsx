e8e7a48b5fe2ceb2595fc00fa968d18a4dc1b278
Brunex7 <brunoromero200@gmail.com>
2023-06-29 18:43:11 -0300

    Modificaciones en App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import { Route, Routes } from "react-router-dom"
-import Home from "./components/Home"
-import Landing from "./components/Landing"
-import About from "./components/About"
-import Projects from "./components/Projects"
-
-
+import { Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import Home from "./components/Home";
+import Landing from "./components/Landing";
+import About from "./components/About";
+import Projects from "./components/Projects";
 
 function App() {
+
+  useEffect(() => {
+    if (window.performance.navigation.type === 1) {
+      window.location.href = "/";
+    }
+  }, []);
 
   return (
     <>
@@ -17,7 +22,7 @@ function App() {
         <Route path="/projects" element={<Projects />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
