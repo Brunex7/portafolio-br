c024069c000b720190d7ecea60583b18dff94e4d
Brunex7 <brunoromero200@gmail.com>
2023-06-29 15:40:49 -0300

    Actulizacion en proyectos

--- src/media/project.js
+++ src/media/project.js
@@ -1,27 +1,28 @@
 import RyM from '../assets/portada RyM.jpg';
 import PI from '../assets/portada PI.jpg';
+import BR from '../assets/portada BR.jpg';
 
 const projects = [
     {
       image: RyM,
       title: 'Rick and Morty App',
-      description: 'Este proyecto se realizo para aplicar los conocimientos del Front-End',
+      description: 'A front-end development project focused solely on enhancing and practicing pure CSS styles and route flow.',
       deployLink: 'https://rick-and-morty-app-sooty.vercel.app/',
       codeLink: 'https://github.com/Brunex7/rick_and_morty',
     },
     {
       image: PI,
       title: 'Henry PI| Videogames',
-      description: 'Proyecto completo de Front-End y Back-End creando la base de datos y todas sus rutas.',
+      description: 'This project was developed during the bootcamp and is a full-stack project, encompassing both front-end and back-end development. Currently, I am working on improving the front-end styles, and the deployment is not enabled yet.',
       deployLink: 'https://example.com/project2-deploy',
       codeLink: 'https://github.com/Brunex7/PI-Videogames-main'
     },
     {
-      image: '',
+      image: BR,
       title: 'Portfolio BR',
-      description: 'Proyecto completo de Front-End y Back-End creando la base de datos y todas sus rutas.',
-      deployLink: 'https://example.com/project2-deploy',
-      codeLink: 'https://github.com/Brunex7/PI-Videogames-main'
+      description: 'You are browsing through it, although I leave here below the display of my previous portfolio, you can see it and compare one project with another.',
+      deployLink: 'https://portafolio-br.vercel.app/',
+      codeLink: 'https://github.com/Brunex7/portafolio-br.git'
     },
 ];
 
