868cc9fcfe7bc43ebfc748385f08df1eb2fe5c02
Brunex7 <brunoromero200@gmail.com>
2023-06-29 15:36:27 -0300

    Cambios en imageGallery

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import imagesGroup from '../media/Images';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
@@ -9,9 +9,12 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 
 function ImageGallery() {
-    const [hoveredIndex, setHoveredIndex] = useState(null);
     const [imageLoaded, setImageLoaded] = useState(false);
-  
+
+    useEffect(() => {
+        setImageLoaded(true);
+    }, []);
+    
     return (
       <Grid
         container
@@ -23,7 +26,8 @@ function ImageGallery() {
         }}
       >
         {imagesGroup.map((subGroup, subGroupIndex) => (
-          <Grid item xs={12} key={subGroupIndex}>
+          <Grid item xs={12} key={subGroupIndex} sx={{opacity: imageLoaded ? 1 : 0, 
+            transition: 'opacity 4.0s'}}>
             <Carousel
               showThumbs={false}
               showStatus={false}
@@ -38,7 +42,7 @@ function ImageGallery() {
                       left: 0,
                       transform: 'translateY(-50%)',
                       zIndex: 2,
-                      color:'#750404'
+                      color: '#750404',  
                     }}
                   >
                     <ArrowBackIosIcon />
@@ -55,7 +59,7 @@ function ImageGallery() {
                       right: 0,
                       transform: 'translateY(-50%)',
                       zIndex: 2,
-                      color:'#750404'
+                      color: '#750404',
                     }}
                   >
                     <ArrowForwardIosIcon />
@@ -71,11 +75,11 @@ function ImageGallery() {
                     position: 'relative',
                     overflow: 'hidden',
                     borderRadius: '18px',
-                    boxShadow: '4px 6px 8px #0f0f0f',
                   }}
-                  onMouseEnter={() => setHoveredIndex(index)}
-                  onMouseLeave={() => setHoveredIndex(null)}
                 >
+                  <Typography variant="h6" style={{ color: '#ffffff' }}>
+                    {image.title}
+                  </Typography>
                   <img
                     src={image.url}
                     alt={image.title}
@@ -83,36 +87,8 @@ function ImageGallery() {
                       width: '100%',
                       height: '100%',
                       objectFit: 'cover',
-                      opacity: imageLoaded ? 1 : 0,
-                      transition: 'opacity 0.4s',
                     }}
-                    onLoad={() => setImageLoaded(true)}
                   />
-                  {hoveredIndex === index && (
-                    <div
-                      style={{
-                        position: 'absolute',
-                        top: 0,
-                        left: 0,
-                        width: '100%',
-                        height: '100%',
-                        background: 'linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))',
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                        opacity: 1,
-                        transition: 'opacity 1.0s',
-                        flexDirection: 'column',
-                      }}
-                    >
-                      <Typography variant="h6" style={{ color: '#ffffff' }}>
-                        {image.title}
-                      </Typography>
-                      <Typography variant="body2" style={{ color: '#ffffff', padding: '10px', marginBottom: '-140px' }}>
-                        {image.description}
-                      </Typography>
-                    </div>
-                  )}
                 </div>
               ))}
             </Carousel>
@@ -120,6 +96,7 @@ function ImageGallery() {
         ))}
       </Grid>
     );
-}
+  }
   
-export default ImageGallery;
+  export default ImageGallery;
+  
