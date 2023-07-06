92a0d27d75cd082967430dd46c50439a0665ecc1
Brunex7 <brunoromero200@gmail.com>
2023-07-06 16:23:30 -0300

    Animacion en image

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -4,7 +4,7 @@ import { Transition } from 'react-transition-group';
 
 import imagesGroup from '../media/Images';
 
-const duration = 1000;
+const duration = 500;
 
 const defaultStyle = {
   transition: `opacity ${duration}ms ease-in-out`,
@@ -17,59 +17,79 @@ const transitionStyles = {
 };
 
 function ImageGallery() {
-
   const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
-    setIsVisible(true);
+    const handleScroll = () => {
+      const element = document.getElementById('image-gallery');
+      if (element) {
+        const rect = element.getBoundingClientRect();
+        const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+        const threshold = windowHeight - rect.height * 0.2;
+        if (rect.top <= threshold && rect.bottom >= 0) {
+          setIsVisible(true);
+        } else {
+          setIsVisible(false);
+        }
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    handleScroll();
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
   }, []);
 
   return (
-    <Transition in={isVisible} timeout={duration}>
-      {(state) => (
-        <Grid
-        container
-      style={{
-        display: 'grid',
-        gridTemplateColumns: 'repeat(auto-fit, minmax(200px, 1fr))',
-        gridGap: '30px',
-        marginTop: '50px',
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        paddingLeft: '30px',
-        paddingRight: '30px',
-        ...defaultStyle,
-        ...transitionStyles[state],
-      }}
-    >
-      {imagesGroup.map((subGroup, subGroupIndex) => (
-        <Grid item xs={12} key={subGroupIndex}>
-          {subGroup.map((image, index) => (
-            <div
-              key={index}
-              style={{
-                aspectRatio: `${image.width} / ${image.height}`,
-                position: 'relative',
-                overflow: 'hidden',
-                borderRadius: '18px',
-              }}
-            >
-              <img
-                src={image.url}
-                alt={image.title}
-                style={{
-                  width: '100%',
-                  height: '100%',
-                  objectFit: 'cover',
-                }}
-              />
-            </div>
-          ))}
-        </Grid>
-      ))}
-    </Grid>
-    )}
-    </Transition>
+    <div id="image-gallery">
+      <Transition in={isVisible} timeout={duration}>
+        {(state) => (
+          <Grid
+            container
+            style={{
+              display: 'grid',
+              gridTemplateColumns: 'repeat(auto-fit, minmax(200px, 1fr))',
+              gridGap: '30px',
+              marginTop: '50px',
+              marginLeft: 'auto',
+              marginRight: 'auto',
+              paddingLeft: '30px',
+              paddingRight: '30px',
+              ...defaultStyle,
+              ...transitionStyles[state],
+            }}
+          >
+            {imagesGroup.map((subGroup, subGroupIndex) => (
+              <Grid item xs={12} key={subGroupIndex}>
+                {subGroup.map((image, index) => (
+                  <div
+                    key={index}
+                    style={{
+                      aspectRatio: `${image.width} / ${image.height}`,
+                      position: 'relative',
+                      overflow: 'hidden',
+                      borderRadius: '18px',
+                    }}
+                  >
+                    <img
+                      src={image.url}
+                      alt={image.title}
+                      style={{
+                        width: '100%',
+                        height: '100%',
+                        objectFit: 'cover',
+                      }}
+                    />
+                  </div>
+                ))}
+              </Grid>
+            ))}
+          </Grid>
+        )}
+      </Transition>
+    </div>
   );
 }
 
